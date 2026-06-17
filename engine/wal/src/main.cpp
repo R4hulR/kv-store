a45a260d0fea0fbb236c81c62061e16ebba81233
Rahul <r4hulr@protonmail.com>
2026-06-17 15:31:39 +0530

    Refactor CMakeLists and implement basic sum functionality in wal module

--- engine/wal/src/main.cpp
+++ engine/wal/src/main.cpp
@@ -1,5 +1,23 @@
 #include <iostream>
+#include "wal.hpp"
 
-int main(){
-    std::cout<<"Hello";
+using namespace std;
+
+int main(int argc, char *argv[]){
+    std::cout<< sum(4,5);
+    // std::string command;
+    // while(cin>>command){
+    //     if (command == "put"){
+    //         string key,value;
+    //         cin>>key>>value;
+    //         // wal.append_put(key,value);
+            
+    //     }
+    //     if (command == "exit"){
+    //         break;
+    //     }
+    // }
+    return 0;
+
+
 }
---
+++ engine/wal/src/wal.cpp
@@ -0,0 +1,5 @@
+#include "wal.hpp"
+int sum(int a,int b){
+    return a+b;
+}
+
